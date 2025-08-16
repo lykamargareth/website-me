5c4ba50f8693c57a8af7c284dd09d852f77e7a42
lykamargareth <159494953+lykamargareth@users.noreply.github.com>
2025-08-16 00:32:11 -0700

    help

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/website-me/',  // must match your repo name, with trailing slash
-  plugins: [react()],
+  base: '/website-me/',  // your repo name with trailing slash
+  plugins: [react()]
 })
