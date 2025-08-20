b9bfc4f30a22741549d50955f2ffed544dada3a3
lykamargareth <159494953+lykamargareth@users.noreply.github.com>
2025-08-20 01:02:11 -0700

    rewrote the background animation
    tried to fix the infinite scroll but couldnt :(

--- src/desktop/Background.jsx
+++ src/desktop/Background.jsx
@@ -1,34 +1,13 @@
-import React from "react";
-import grass from "../assets/Grass.png";
-import cloudsFront from "../assets/Clouds-front.png";
-import cloudsMid from "../assets/Clouds-mid.png";
-import cloudsBack from "../assets/Clouds-back.png";
-import sky from "../assets/Background.png";
-// import "./Background.css";
+
 
 function Background() {
   return (
     <div className="background-container">
-      <div className="background-layer sky">
-        <img src={sky} alt="sky" />
-        <img src={sky} alt="sky duplicate" />
-      </div>
-      <div className="background-layer clouds-back">
-        <img src={cloudsBack} alt="clouds back" />
-        <img src={cloudsBack} alt="clouds back duplicate" />
-      </div>
-      <div className="background-layer clouds-mid">
-        <img src={cloudsMid} alt="clouds mid" />
-        <img src={cloudsMid} alt="clouds mid duplicate" />
-      </div>
-      <div className="background-layer clouds-front">
-        <img src={cloudsFront} alt="clouds front" />
-        <img src={cloudsFront} alt="clouds front duplicate" />
-      </div>
-      <div className="background-layer grass">
-        <img src={grass} alt="grass" />
-        <img src={grass} alt="grass duplicate" />
-      </div>
+      <div className="background-layer sky"></div>
+      <div className="background-layer clouds-back"></div>
+      <div className="background-layer clouds-mid"></div>
+      <div className="background-layer clouds-front"></div>
+      <div className="background-layer grass"></div>
     </div>
   );
 }
