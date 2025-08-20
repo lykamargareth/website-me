262048643fea1b7e85d71225bffba98dcf61899c
lykamargareth <159494953+lykamargareth@users.noreply.github.com>
2025-08-20 01:31:22 -0700

    used parallax package to fix scrolling
    totally forgot about packages lol

--- src/desktop/Background.jsx
+++ src/desktop/Background.jsx
@@ -1,13 +1,52 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
 
+import sky from "../assets/Background.png";
+import cloudsBack from "../assets/Clouds-back.png";
+import cloudsMid from "../assets/Clouds-mid.png";
+import cloudsFront from "../assets/Clouds-front.png";
+import grass from "../assets/Grass.png";
+
+const layers = [
+  { src: sky, speed: 5, zIndex: 1, alt: "sky" },
+  { src: cloudsBack, speed: 10, zIndex: 2, alt: "clouds back" },
+  { src: cloudsMid, speed: 20, zIndex: 3, alt: "clouds mid" },
+  { src: cloudsFront, speed: 40, zIndex: 4, alt: "clouds front" },
+  { src: grass, speed: 80, zIndex: 5, alt: "grass" },
+];
 
 function Background() {
   return (
-    <div className="background-container">
-      <div className="background-layer sky"></div>
-      <div className="background-layer clouds-back"></div>
-      <div className="background-layer clouds-mid"></div>
-      <div className="background-layer clouds-front"></div>
-      <div className="background-layer grass"></div>
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        zIndex: -1,
+        overflow: "hidden",
+      }}
+    >
+      {layers.map(({ src, speed, zIndex, alt }) => (
+        <Marquee
+          key={alt}
+          direction="left"
+          speed={speed}
+          gradient={false}
+          pauseOnHover={false}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            zIndex,
+          }}
+        >
+          <img src={src} alt={alt} style={{ height: "100vh" }} />
+        </Marquee>
+      ))}
     </div>
   );
 }
